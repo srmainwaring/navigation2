30def92c04aa68acb706a667e7b40380d8662df4
Rhys Mainwaring <rhys.mainwaring@me.com>
2024-01-15 10:49:35 +0000

    nav2_behaviour_tree: fix for compiling with clang
    - Resolve error:  use 'template' keyword to treat 'set' as a dependent template name
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- nav2_behavior_tree/include/nav2_behavior_tree/bt_action_server_impl.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/bt_action_server_impl.hpp
@@ -187,9 +187,9 @@ bool BtActionServer<ActionT>::loadBehaviorTree(const std::string & bt_xml_filena
   try {
     tree_ = bt_->createTreeFromText(xml_string, blackboard_);
     for (auto & blackboard : tree_.blackboard_stack) {
-      blackboard->set<rclcpp::Node::SharedPtr>("node", client_node_);
-      blackboard->set<std::chrono::milliseconds>("server_timeout", default_server_timeout_);
-      blackboard->set<std::chrono::milliseconds>("bt_loop_duration", bt_loop_duration_);
+      blackboard->template set<rclcpp::Node::SharedPtr>("node", client_node_);
+      blackboard->template set<std::chrono::milliseconds>("server_timeout", default_server_timeout_);
+      blackboard->template set<std::chrono::milliseconds>("bt_loop_duration", bt_loop_duration_);
     }
   } catch (const std::exception & e) {
     RCLCPP_ERROR(logger_, "Exception when loading BT: %s", e.what());
